7d543454f667b5965c17a97fcf31b483b3a30cac
Dennis <mwendadennis8779@gmail.com>
2022-11-28 11:52:13 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = function(){
+    let num1 = 2000
+    let num2 = 100
+    return num1 / num2
+    
+}
+const square = (x) => x*x;
+square(x);
+
+const add =(a,b) => a + b;
+add(a, b);
+    
+
+
